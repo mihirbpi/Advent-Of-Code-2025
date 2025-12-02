236154c02f27a688749ae5f8d49a8c1c980511d4
mihirbpi <mihir.borkar@gmail.com>
2025-12-02 13:04:52 -0800

    formatting

--- adv02/adv2-1.py
+++ adv02/adv2-1.py
@@ -3,12 +3,9 @@ data = get_data(year=2025, day=2).split(",")
 
 def get_invalid_ids(start, end):
     ids = set()
-    
     for n in range(start, end+1):
         s = str(n)
-        
         for l in range(1,len(s)):
-            
             if (s == 2*s[:l]):
                 ids.add(n)
                 break
@@ -19,5 +16,4 @@ ans = 0
 for data_string in data:
     invalid_ids = get_invalid_ids(*[int(x) for x in data_string.split("-")])
     ans += sum(invalid_ids)
-        
 print(ans)
--- adv02/adv2-2.py
+++ adv02/adv2-2.py
@@ -3,29 +3,21 @@ data = get_data(year=2025, day=2).split(",")
 
 def get_invalid_ids(start, end):
     ids = set()
-    
     for n in range(start, end+1):
         s = str(n)
-        
         for l in range(1,len(s)):
             found = False
-            
             for k in range(2,len(s)+1):
-                
                 if (s == k*s[:l]):
                     ids.add(n)
                     found = True
                     break
-                
             if (found):
                 break
     return list(ids)
         
-
 ans = 0
-
 for data_string in data:
     invalid_ids = get_invalid_ids(*[int(x) for x in data_string.split("-")])
-    ans += sum(invalid_ids)
-        
+    ans += sum(invalid_ids)   
 print(ans)   
