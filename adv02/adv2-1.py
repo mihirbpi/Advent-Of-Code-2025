9ccfebb1e6aec6d70d1cacdf792f5ce39ae8e289
mihirbpi <mihir.borkar@gmail.com>
2025-12-02 13:00:48 -0800

    Finished Day 2

---
+++ adv02/adv2-1.py
@@ -0,0 +1,23 @@
+from aocd import get_data
+data = get_data(year=2025, day=2).split(",")
+
+def get_invalid_ids(start, end):
+    ids = set()
+    
+    for n in range(start, end+1):
+        s = str(n)
+        
+        for l in range(1,len(s)):
+            
+            if (s == 2*s[:l]):
+                ids.add(n)
+                break
+    return list(ids)
+                
+ans = 0
+
+for data_string in data:
+    invalid_ids = get_invalid_ids(*[int(x) for x in data_string.split("-")])
+    ans += sum(invalid_ids)
+        
+print(ans)
