61ceaca6e5b82830b145496c9de52ab5792651f9
mihirbpi <mihir.borkar@gmail.com>
2025-12-07 11:53:57 -0800

    remove unneeded visited

--- adv07/adv7-2.py
+++ adv07/adv7-2.py
@@ -2,7 +2,6 @@ from aocd import get_data
 from functools import cache
 from collections import defaultdict
 data = get_data(year=2025, day=7).split("\n")
-
 grid = defaultdict(lambda: 0)
 
 for row in range(len(data)):
@@ -22,10 +21,9 @@ def get_num_paths(start_pos):
     
     if ((row >= len(data)-1)):
         return 1
-    if (grid[start_pos] == '^'):
+    elif (grid[start_pos] == '^'):
         return get_num_paths((row,col-1))+get_num_paths((row,col+1))
     else:
         return get_num_paths((row+1,col))
         
-visited = set()
 print(get_num_paths(start))
